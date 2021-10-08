92febaa8b7de8771bba42b4946910a336d13ccd2
AdamMatheus <adam.matheus.it@gmail.com>
2021-10-08 19:50:22 +0200

    reverse added

---
+++ python7/reverse word.py
@@ -0,0 +1,9 @@
+def tersyaz(soz):
+	return ''.join(reversed(soz))#"".join(reverse())
+print(tersyaz('Hayirli Cumalar'))
+
+
+# "".join(reversed("Python"))
+
+# s = 'you shall not pass'
+# r = ' '.join(reversed(s.split(' ')))
