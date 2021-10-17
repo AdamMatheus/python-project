77929de76d96c518f4e8506e06c0651cb274d1cb
AdamMatheus <adam.matheus.it@gmail.com>
2021-10-17 21:11:10 +0200

    added file

---
+++ python7/examples1.py
@@ -0,0 +1,23 @@
+# n=876509755433 #######3sayiyi ters listeleme
+# print(list(str(n))[::-1])
+
+# from math import factorial
+# print(factorial(4))
+
+# def fakt(n):
+#     sonuc=1
+#     for i in range(1,n+1):
+#         sonuc*=i
+#     return sonuc
+# print(fakt(5))
+
+###print((lambda a,b:a+b)(3,5))
+# building a calculator by using Dict with Lambda expression
+
+
+# ops={"+":(lambda x,y:x+y),"-":(lambda x,y:x-y),"*":(lambda x,y:x*y),"/":(lambda x,y:x/y)}
+# print(ops["-"](4,6))
+
+
+
+
