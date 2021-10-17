f4551fc4005bea2b183b1333a570f41c9bcecfad
AdamMatheus <adam.matheus.it@gmail.com>
2021-10-17 21:05:42 +0200

    file added

---
+++ python7/modules.py
@@ -0,0 +1,36 @@
+# import math
+# print(dir(math))
+
+# from math import pi
+# print(pi)
+# from math import factorial
+# print(factorial(3))
+# from math import log10
+# print(log10(1000))
+# from math import sin
+# print(sin(pi/2))
+
+
+#######################import string######
+# import string
+# #print(dir(string))
+# print(string.punctuation) #####3noktalama isaretleri
+# print(string.digits)
+
+########### datetime #######
+# import datetime
+# print(dir(datetime))
+
+# print(datetime.date.today())
+# print(datetime.datetime.now())
+
+
+# from datetime import date
+# birth=date(571,4,22)
+# death=date(632,6,8)
+# print(death-birth)
+# print(date.toordinal(death)-date.toordinal(birth))
+
+import os
+cwd = os.getcwd()
+print(cwd)
